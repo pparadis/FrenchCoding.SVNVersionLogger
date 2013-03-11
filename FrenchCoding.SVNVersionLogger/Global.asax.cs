a99f465ad96800bab557cd6c35fea30110bdd522
Pascal Paradis <pascal.paradis@gmail.com>
2013-03-11 14:27:05 -0400

    Test de Visual Studio

--- FrenchCoding.SVNVersionLogger/Global.asax.cs
+++ FrenchCoding.SVNVersionLogger/Global.asax.cs
@@ -22,6 +22,7 @@ namespace FrenchCoding.SVNVersionLogger
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
